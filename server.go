a40cf82e9e9de539fffbbe9eb779b01f04914b79
caohieu04 <41721703+caohieu04@users.noreply.github.com>
2021-09-09 18:06:09 +0700

    binding-validation

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"net/http"
 	"os"
 
 	"github.com/caohieu04/Gin-Course/controller"
@@ -32,7 +33,12 @@ func main() {
 		ctx.JSON(200, videoController.FindAll())
 	})
 	server.POST("/videos", func(ctx *gin.Context) {
-		ctx.JSON(200, videoController.Save(ctx))
+		err := videoController.Save(ctx)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		} else {
+			ctx.JSON(http.StatusOK, gin.H{"message": "Video Input is Valid!!"})
+		}
 	})
 
 	server.Run("127.0.0.1:8080")
